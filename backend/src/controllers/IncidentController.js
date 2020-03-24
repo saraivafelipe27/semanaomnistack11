001af27d4179de0fd397969e9e2f2e0c4e5e1499
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-03-24 19:27:07 -0300

    Feature: rota delete dos casos(incidents) criada

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -19,5 +19,23 @@ module.exports = {
         });
 
         return res.json({ id });
+    },
+
+    async delete(req, res) {
+      const { id } = req.params; 
+      const ong_id = req.headers.authorization;
+
+      const incidents = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first()
+        
+        if (incidents.ong_id !== ong_id) {
+            return res.status(401).json({ error: 'Operation not permitted.' });
+        }
+
+        await connection('incidents').where('id', id).delete();
+
+        return res.status(204).send();
     }
 };
