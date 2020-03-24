30df12f37d3ea77ec861e70506bd76fafafc1c67
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-03-24 17:12:00 -0300

    chore: criada rota de listagem das ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,12 @@ const connection = require('./database/connection');
 
 const routes = express.Router();
 
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*');
+
+    return res.json(ongs);
+})
+
 routes.post('/ongs', async (req, res) => {
     const { name, email, whatsapp, city, uf } = req.body;
     
@@ -16,7 +22,7 @@ routes.post('/ongs', async (req, res) => {
         email,
         whatsapp,
         city,
-        uf
+        uf,
     })
 
     return res.json({ id });
